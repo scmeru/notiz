7f42d27102a015bf0accc00d8a983c4da4dbc409
mandazwt <zuwitamanda@gmail.com>
2025-05-27 01:29:09 +0700

    javascript fix

--- footer.php
+++ footer.php
@@ -10,8 +10,7 @@
 </div>
 
 <!-- Bootstrap JS (with Popper) from CDN -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Custom Search Script -->
 <script>
